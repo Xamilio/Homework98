88cf888bcb7d63c5a8be0f2e01486b8cbe7cb1d0
Xamilio25 <Xamilio25@XAMILIO>
2024-10-25 11:06:27 +0300

    Добавьте файлы проекта.

---
+++ prak/prak.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int n = 1;
+	int k = 0;
+	cout << "Enter x: ";
+	cin >> x;
+	for (int i = 0; i < x; i++)
+	{
+		k++;
+		for (int j = 0; j < k; j++)
+		{
+			cout << n << " ";
+		}
+		n++;
+		cout << endl;
+	}
+}
